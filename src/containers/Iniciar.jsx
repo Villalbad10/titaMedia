6f56419438a93c77528d07e36181d87c4ca40bb4
Diego Villalba <diego.villalba01@uptc.edu.co>
2022-08-20 12:14:18 -0500

    login mibile and desktop

--- src/containers/Iniciar.jsx
+++ src/containers/Iniciar.jsx
@@ -5,19 +5,24 @@ import IniciaCon from '../components/IniciaCon';
 const Iniciar = () => {
 
    return (
-      <div className='container'>
-         <img className='logo' src="https://res.cloudinary.com/villalbad10/image/upload/v1661000233/titaMedia/Group_9_1_lczed4.png" alt="titanet" />
-         <img className='center' src="https://res.cloudinary.com/villalbad10/image/upload/v1661001228/titaMedia/object_hmtc9y.png" alt="" />
-         <h1 className='login-text'>Iniciar sesión</h1>
+      <section className='container-login'>
+         <div className='loginOpciones'>
+            <img className='logo' src="https://res.cloudinary.com/villalbad10/image/upload/v1661000233/titaMedia/Group_9_1_lczed4.png" alt="titanet" />
+            <img className='center logoLogin' src="https://res.cloudinary.com/villalbad10/image/upload/v1661001228/titaMedia/object_hmtc9y.png" alt="" />
+            <h1 className='login-text'>Iniciar sesión</h1>
 
-         <form className='formLogin'>
-            <input className='input-login' type="email" name="" required placeholder='Correo' /> <br />
-            <input className='input-login' type="password" name="" required placeholder='Contraseña' />
-            <div className='contLogin'> <Boton text={'Iniciar Sesión'} /></div>
-            <IniciaCon img='https://res.cloudinary.com/villalbad10/image/upload/v1661009700/titaMedia/google_avbjfd.png' text='Goolge' />
-            <IniciaCon img='https://res.cloudinary.com/villalbad10/image/upload/v1661009700/titaMedia/facebook_amuwqc.png' text='Facebook' />
-         </form>
-      </div>
+            <form className='formLogin'>
+               <input className='input-login' type="email" name="" required placeholder='Correo' /> <br />
+               <input className='input-login' type="password" name="" required placeholder='Contraseña' />
+               <div className='contLogin'> <Boton text={'Iniciar Sesión'} /></div>
+               <IniciaCon img='https://res.cloudinary.com/villalbad10/image/upload/v1661009700/titaMedia/google_avbjfd.png' text='Goolge' />
+               <IniciaCon img='https://res.cloudinary.com/villalbad10/image/upload/v1661009700/titaMedia/facebook_amuwqc.png' text='Facebook' />
+            </form>
+         </div>
+         <div className='container-img'>
+            <img className='center' src="https://res.cloudinary.com/villalbad10/image/upload/v1661013468/titaMedia/loginLogo_k6yfll.png" />
+         </div>
+      </section>
    )
 }
 
